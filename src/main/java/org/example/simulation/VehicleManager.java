9c8ff81541bafddde03646e31674f3066f62f6bd
AstroCorpo <pietrek2911@gmail.com>
2025-05-22 11:15:42 +0200

    vehicle_manager_v1

--- src/main/java/org/example/simulation/VehicleManager.java
+++ src/main/java/org/example/simulation/VehicleManager.java
@@ -1,4 +1,21 @@
 package org.example.simulation;
 
+import org.example.model.IntersectionState;
+import org.example.model.light.Signal;
+import org.example.model.vehicle.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehicleManager {
+
+    public void step(IntersectionState intersectionState) {
+
+        var readyToGo = intersectionState.getIntersectionLightsState()
+                .keySet().stream().filter(key -> intersectionState.getIntersectionLightsState().getOrDefault(key, Signal.RED) == Signal.GREEN)
+                .filter(key -> intersectionState.getWaitingVehicles().containsKey(key))
+                .flatMap(key -> intersectionState.getWaitingVehicles().get(key).stream()).toList();
+
+    }
+
 }
